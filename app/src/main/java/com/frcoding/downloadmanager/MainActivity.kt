589d47e1a41ca65f96d4a5090ff8b565e33cae9c
atifereydooni <am.fereydooni@gmail.com>
2023-02-05 01:27:52 +0100

    Use download manager to download a file

--- app/src/main/java/com/frcoding/downloadmanager/MainActivity.kt
+++ app/src/main/java/com/frcoding/downloadmanager/MainActivity.kt
@@ -3,17 +3,14 @@ package com.frcoding.downloadmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.frcoding.downloadmanager.ui.theme.DownloadManagerTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val downloader = AndroidDownloader(this)
+        downloader.downloadFile("https://developer-blogs.nvidia.com/wp-content/uploads/2016/07/cute.jpg")
+
         super.onCreate(savedInstanceState)
         setContent {
             DownloadManagerTheme {
